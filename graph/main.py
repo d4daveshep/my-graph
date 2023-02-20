11537113df725730575f1cd2a2e76d9822314972
d4daveshep <david@daveshep.net.nz>
2023-02-21 09:15:09 +1300

    parsing staff_list.csv file

--- graph/main.py
+++ graph/main.py
@@ -1,2 +1,23 @@
+from graph.graph import Graph
+
+def parse_line(line):
+    data = line.strip().split(',')
+    return tuple(data[:3])
+
+
 if __name__ == "__main__":
     print("In main")
+
+    graph = Graph()
+
+    with open("./staff_list.csv") as csv_file:
+        line = csv_file.readline()
+        while line:
+            name, gm, employment_type = parse_line(line)
+            graph.add_relationship(name,"under GM",gm)
+            graph.add_relationship(name,"employment",employment_type)
+            line = csv_file.readline()
+
+    pass
+
+
