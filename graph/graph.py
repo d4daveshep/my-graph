2e9ef93d78751a38f69e5ebf00eb3afd275cc144
d4daveshep <david@daveshep.net.nz>
2023-02-20 13:43:38 +1300

    working on node tests

--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,16 @@
+from graph.node import Node
+
+
 class Graph:
     def __init__(self):
         self.relationships = []
+        self.nodes = {}
 
     def add_relationship(self, subject_node: str, relationship: str, object_node: str) -> None:
+        pass
+
+    def add_node(self, name:str):
+        if name not in self.nodes:
+            self.nodes[name] = Node(name)
+
 
--- tests/test-graph.py
+++ tests/test-graph.py
@@ -1,7 +1,15 @@
 import pytest
 
 from graph.graph import Graph
+from graph.node import Node
 
+
+def test_create_nodes():
+    graph = Graph()
+    graph.add_node("Andrew")
+
+    assert "Andrew" in graph.nodes
+    # assert len(andrew.connections) == 0
 
 def test_create_relationship():
     graph = Graph()
